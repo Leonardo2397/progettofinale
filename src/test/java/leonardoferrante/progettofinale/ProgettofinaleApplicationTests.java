d00a70f93c535a0d14fa2c0fb6914e89687420c5
Leonardo2397 <leonardoferrante753@gmail.com>
2025-11-11 10:34:39 +0100

    Initial commit

---
+++ src/test/java/leonardoferrante/progettofinale/ProgettofinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardoferrante.progettofinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettofinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
