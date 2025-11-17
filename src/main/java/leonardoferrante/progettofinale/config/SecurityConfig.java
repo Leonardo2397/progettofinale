260f678c27ff91bfa031ad7cd1ae5aaa5a96a591
Leonardo2397 <leonardoferrante753@gmail.com>
2025-11-17 11:22:36 +0100

    edit

--- src/main/java/leonardoferrante/progettofinale/config/SecurityConfig.java
+++ src/main/java/leonardoferrante/progettofinale/config/SecurityConfig.java
@@ -1,50 +1,3 @@
-//package leonardoferrante.progettofinale.config;
-//
-//
-//import leonardoferrante.progettofinale.security.JwtAuthenticationFilter;
-//import leonardoferrante.progettofinale.services.impl.CustomUserDetailsService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-//    private final CustomUserDetailsService customUserDetailsService;
-//
-//
-//    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomUserDetailsService customUserDetailsService) {
-//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-//        this.customUserDetailsService = customUserDetailsService;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/auth/**").permitAll()
-//                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-
 package leonardoferrante.progettofinale.config;
 
 import leonardoferrante.progettofinale.security.JwtAuthenticationFilter;
@@ -79,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
